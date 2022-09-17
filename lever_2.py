11872da18d3c99e77b7262589c4480092d8bf792
eunwool <leunwoo15@gmail.com>
2022-09-17 10:35:36 +0900

    #

---
+++ lever_2.py
@@ -0,0 +1,23 @@
+# # 숫자의 표현
+# 'https://school.programmers.co.kr/learn/courses/30/lessons/12924'
+# def solution(n):
+#     result = 0
+#     for i in range(1, n + 1):
+#         a = 0
+#         while True:
+#             a += i
+#             i += 1
+#             if a > n: break
+#             if a == n: result += 1
+#     return result
+
+# # 영어 끝말잇기
+# 'https://school.programmers.co.kr/learn/courses/30/lessons/12981'
+# def solution(n, words):
+#     pass
+
+# n = 3
+# words = ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
+# print(solution(n, words))
+
+# #
